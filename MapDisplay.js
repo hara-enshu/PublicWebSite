d9bf9ca2038c80800a87e8db6105793d9db85480
hara <84570019+hara-enshu@users.noreply.github.com>
2021-10-08 08:52:48 +0900

    一部のラジオボタンに地図移動追加

--- MapDisplay.js
+++ MapDisplay.js
@@ -5,6 +5,23 @@ var map = L.map('mapid', {
     zoom: 13,
 });
 
+/* 地図の移動 */
+function MoveChuo(){        //四国中央市
+    map.setView([33.980415, 133.54903]);
+}
+function MoveNiihama(){     //新居浜市
+    map.setView([33.960288, 133.283392]);
+}
+function MoveSaijo(){       //西条市
+    map.setView([33.919495, 133.181144]);
+}
+function MoveImabari(){     //今治市
+    map.setView([34.066202, 132.997791]);
+}
+function MoveMatsuyama(){   //松山市
+    map.setView([33.839165, 132.76564]);
+}
+
 //地図データ取得
 var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
@@ -16,7 +33,7 @@ var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     latlng[1] : 緯度
     latlng[0] : 経度
 */
-function getLatLng1(){
+function GetLatLng1(){
     var address = $('#address1').val();
         $.ajax({
             url:'https://msearch.gsi.go.jp/address-search/AddressSearch?q=' + address,
